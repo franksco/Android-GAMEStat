2dce52ea439b419a7d771a515a49c231a82ce5cf
Cory Franks <franksco39@gmail.com>
2016-07-08 18:36:09 -0700

    finished developer model, but its now throughing an error saying it cant find symbol variable my api key and its coming from my constants.java

--- app/src/main/java/com/epicodus/gamestat/Developer.java
+++ app/src/main/java/com/epicodus/gamestat/Developer.java
@@ -6,38 +6,19 @@ import java.util.ArrayList;
 public class Developer {
     private String mName;
     private String mDeck;
+    private String mFounded;
     private String mId;
-    private String mGenres;
-    private String mImageUrl;
-    private String mReleaseDate;
-    private ArrayList<Game> mGames = new ArrayList<>();
+    private String mWebsite;
 
-    public Developer(String name, String deck, String id, String imageUrl) {
+    private String mCountry;
+    private String mCity;
+
+    public Developer(String name, String id, String website) {
         this.mName = name;
-        this.mDeck = deck;
         this.mId = id;
-        this.mImageUrl = imageUrl;
+        this.mWebsite = website;
     }
 
-    public Developer(String name, String deck, String id, String genres,
-                String imageUrl) {
-        this.mName = name;
-        this.mDeck = deck;
-        this.mId = id;
-        this.mGenres = genres;
-        this.mImageUrl = imageUrl;
-    }
-
-    public Developer(String name, String deck, String id, String genres,
-                String imageUrl, String releaseDate, ArrayList<Developer> developers) {
-        this.mName = name;
-        this.mDeck = deck;
-        this.mId = id;
-        this.mGenres = genres;
-        this.mImageUrl = imageUrl;
-        this.mReleaseDate = releaseDate;
-        this.mDevelopers = developers;
-    }
 
     public String getName() {
         return mName;
@@ -47,26 +28,41 @@ public class Developer {
         return mDeck;
     }
 
+    public String getFounded() {
+        return mFounded;
+    }
+
     public String getId() {
         return mId;
     }
 
-    public String getGenres() {
-        return mGenres;
+    public String getWebsite() {
+        return mWebsite;
     }
 
-    public String getImageUrl() {
-        return mImageUrl;
+    public String getCountry() {
+        return mCountry;
     }
 
-    public String getReleaseDate() {
-        return mReleaseDate;
-    }
-
-    public ArrayList<Developer> getDevelopers() {
-        return mDevelopers;
+    public String getCity() {
+        return mCity;
     }
 
 }
 
-}
+//    private ArrayList<DevelopedGames> mDevelopedGames = new ArrayList<>();
+//
+//    public ArrayList<DevelopedGames> getDevelopedGames() {
+//        return mDevelopedGames;
+//    }
+//
+//    public Developer(String name, String deck, String founded, String id, String website, ArrayList<DevelopedGames> developedgames, String country, String city) {
+//        this.mName = name;
+//        this.mDeck = deck;
+//        this.mFounded = founded;
+//        this.mId = id;
+//        this.mWebsite = website;
+//        this.mDevelopedGames = developedgames;
+//        this.mCountry = country;
+//        this.mCity = city;
+//    }
